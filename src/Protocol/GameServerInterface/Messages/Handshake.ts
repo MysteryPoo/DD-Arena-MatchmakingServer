398c3514434bc45efd201d910acae3612c4d1415
MysteryPoo <Matt.Thornton@knights.ucf.edu>
2020-06-14 19:32:08 -0400

    First phase of protocol conversion for GameServer interface

--- src/Protocol/GameServerInterface/Messages/Handshake.ts
+++ src/Protocol/GameServerInterface/Messages/Handshake.ts
@@ -1,6 +1,5 @@
 
 import { MessageBase } from "../../../Abstracts/MessageBase";
-import { BufferHelper } from "../../../BufferHelper";
 
 export class Handshake extends MessageBase {
 
@@ -10,55 +9,25 @@ export class Handshake extends MessageBase {
     public playerTokenList!: Array<number>;
 
     serialize(): string {
-        throw("");
-        if (this.playerIdList.length !== this.playerTokenList.length) {
-            console.debug(`playerIdList.length = ${this.playerIdList.length}`);
-            console.debug(`playerTokenList.length = ${this.playerTokenList.length}`);
-            throw new Error("Array sizes must match.");
-        }
-        let staticBufferSize : number = 6 + 2 * this.playerTokenList.length + this.playerIdList.length;
-        let idListLength : number = 0;
-        for (let id of this.playerIdList) {
-            idListLength += Buffer.byteLength(id, 'utf-8');
-        }
-        let bufferSize : number = staticBufferSize + idListLength;
-        let helper : BufferHelper = new BufferHelper(Buffer.allocUnsafe(bufferSize));
-
-        helper.writeUInt8(this.messageId);
-        helper.writeUInt32LE(bufferSize);
-        helper.writeUInt8(this.playerIdList.length);
-        for (let id of this.playerIdList) {
-            helper.writeUInt8(Buffer.byteLength(id, 'utf-8'));
-            helper.writeString(id);
-        }
-        for (let token of this.playerTokenList) {
-            helper.writeUInt16LE(token);
-        }
-
+        return JSON.stringify({
+            messageId : this.messageId,
+            playerIdList : this.playerIdList,
+            playerTokenList : this.playerTokenList
+        });
     }
 
     deserialize(data: string): void {
-        // try {
-        //     let helper : BufferHelper = new BufferHelper(data);
-        //     this.gameVersion = helper.readUInt8();
-            
-        //     let passLength : number = helper.readUInt8();
-        //     this.gameServerPassword = helper.readString(passLength);
+        try {
+            let message : any = JSON.parse(data);
 
-        //     let numPlayers : number = helper.readUInt8();
-        //     if (this.playerIdList === undefined) {
-        //         this.playerIdList = [];
-        //     }
-        //     for (let p = 0; p < numPlayers; ++p) {
-        //         let idLength : number = helper.readUInt8();
-        //         this.playerIdList.push(helper.readString(idLength));
-        //     }
+            this.gameVersion = message.gameVersion;
+            this.gameServerPassword = message.gameServerPassword;
+            this.playerIdList = message.playerIdList;
 
-        //     this.valid = true;
-        // } catch (e) {
-        //     console.error(e);
-        //     this.valid = false;
-        // }
+            this.valid = true;
+        } catch (e) {
+            this.valid = false;
+        }
     }
 
 }
