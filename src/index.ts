4a455553a87a437237bbfe321360cfff04e23a1d
MysteryPoo <Matt.Thornton@knights.ucf.edu>
2020-06-16 11:39:58 -0400

    Corrected reference of port for Game Server to connect to

--- src/index.ts
+++ src/index.ts
@@ -9,8 +9,8 @@ import { ContainerManager } from "./ContainerManager";
 
 config();
 
-const gameClientPort : number = Number(process.env.PORT);
-const gameServerPort : number = Number(process.env.GSPORT);
+const gameClientPort : number = process.env.PORT ? Number(process.env.PORT) : 40000;
+const gameServerPort : number = process.env.GSPORT ? Number(process.env.GSPORT) : 40001;
 
 mongoose.connect('mongodb://' + process.env.MONGODB + ':27017/' + process.env.DB, {useNewUrlParser: true});
 var db = mongoose.connection;
@@ -22,11 +22,10 @@ db.once('open', function() {
 
 // TODO : All this hostname/port stuff needs to get refactored to support multiple docker hosts
 const hostname : string = process.env.MMSIP ? process.env.MMSIP : "localhost";
-const port : number = 9001;
 const serverPoolSize : number = process.env.SERVERPOOLSIZE ? Number(process.env.SERVERPOOLSIZE) : 5;
 const serverPortFirst : number = process.env.SERVERPORTFIRST ? Number(process.env.SERVERPORTFIRST) : 40002;
 
-const containerManager : ContainerManager = new ContainerManager(hostname, port, serverPoolSize, serverPortFirst);
+const containerManager : ContainerManager = new ContainerManager(hostname, gameServerPort, serverPoolSize, serverPortFirst);
 const lobbyManager : LobbyManager = new LobbyManager(containerManager, hostname);
 
 const server : UserServerManager = new UserServerManager(lobbyManager);
@@ -34,6 +33,3 @@ server.start(gameClientPort);
 
 const gameServerManager : GameServerServer = new GameServerServer(lobbyManager);
 gameServerManager.start(gameServerPort);
-
-
-
